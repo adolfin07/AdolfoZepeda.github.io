2b81b21adc8895da51576b66a4a18a1d0778b7f9
AdolfoZepeda <adolfitozepeda@gmail.com>
2025-03-05 22:04:53 -0700

    second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+const LIVE_URL = "https://github.com/adolfin07/AdolfoZepeda.github.io"
 
 // https://astro.build/config
 export default defineConfig({
