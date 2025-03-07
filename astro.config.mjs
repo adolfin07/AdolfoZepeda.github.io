bd0407ee72cb0d25bb324d67078ddd1a317b4250
AdolfoZepeda <adolfitozepeda@gmail.com>
2025-03-07 14:44:28 -0700

    maybe resolve

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,3 @@
-// @ts-check
-
 
 import { defineConfig } from 'astro/config';
 
@@ -8,10 +6,10 @@ const LIVE_URL = "https://github.com/adolfin07/AdolfoZepeda.github.io"
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-  site: "https://github.com/adolfin07",
-  base: "AdolfoZv"
+  site: "https://adolfozepeda.github.io", // Reemplaza con tu URL correcta
+  base: "/AdolfoZepeda.github.io/", // Ajusta el base si es necesario
+
 });
+
+
 
